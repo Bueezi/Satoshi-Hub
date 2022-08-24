10892d731ac206e35e1b16ed4f44ae5336f6a0fd
Bueezi <66019436+Bueezi@users.noreply.github.com>
2022-08-25 01:19:03 +0200

    Update Teleport.lua

--- Teleport.lua
+++ Teleport.lua
@@ -1,35 +1,35 @@
-function tp(x,y,z)
+function tp(x,y,z,Debug)
 moving = true
 if x < game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X then
 while x < game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X do
 wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X-valtomove,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z))
-print(" Adjusting X")
+if Debug == true then print(" Adjusting X") end
 end
 end
 if z < game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z then
 while z < game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z do
 wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z-valtomove))
-print(" Adjusting Z")
+if Debug == true then print(" Adjusting Z") end
 end
 end
 if x > game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X then
 while x > game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X do
 wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X+valtomove,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z))
-print("Micro Adjusting X")
+if Debug == true then print("Micro Adjusting X") end
 end
 end
 if z > game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z then
 while z > game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z do
 wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y,game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z+valtomove))
-print("Micro Adjusting Z")
+if Debug == true then print("Micro Adjusting Z") end
 end
 end
 moving = false
-print("Ended")
+if Debug == true then print("Teleportation Ended") end
 end
  
 
